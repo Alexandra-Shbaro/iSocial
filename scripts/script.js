c561b83362071ec0e53530d92803306b134a519b
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-11-05 01:19:28 +0200

    adding expertise and why us initial structure and design

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Animate main heading
     gsap.from(".hero h1:first-of-type", {
         y: 50,
@@ -42,4 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
         delay: 1.5,
         ease: "power3.out"
     });
+
+    
 });
+
+
